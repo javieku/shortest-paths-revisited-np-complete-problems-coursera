68c9ee506d8bd01f61dab04605c4f7e7b36b0b6f
javieku <javieku@gmail.com>
2018-09-16 12:21:41 +0200

    Rename Bellman-Ford file and move scripts common code to a separate file

--- week1/bellman_ford_shortest_path.rb
+++ week1/bellman_ford_shortest_path.rb
@@ -1,15 +1,13 @@
-require 'singleton'
+require_relative 'common'
 require_relative 'graph'
 
-FIXNUM_MAX = (2**(0.size * 8 -2) -1)
-FIXNUM_MIN = -(2**(0.size * 8 -2))
-
 class BellmanFordAlgorithm
-
   attr_reader :shortest_path
+  attr_reader :has_negative_cycle
 
   def initialize
     @shortest_path = []
+    @has_negative_cycle = false
   end
 
   def execute graph, initial_vertex
@@ -38,36 +36,23 @@ class BellmanFordAlgorithm
         end
     end
 
+    for v in (1..n)
+        edge = graph.predecessors(v).min_by { |pred| pred.weight }
+        if(edge == nil)
+            result[n][v] = result[n-1][v] 
+        else
+            result[n][v] = [result[n-1][v], result[n-1][edge.src] + edge.weight].min 
+        end
+    end
+    
+    @has_negative_cycle = result[n] != result[n-1];
     @shortest_path = result[n-1];
-  end
-end
-
-class GraphLoader
-  include Singleton
-  def read_graph filename
-    graph = Graph.new
-    File.open(filename, "r") do |f|
-        f.each_with_index do |line,index|
-            if (index == 0)
-                next
-            else
-                edge = line.split(/\s/).reject(&:empty?)
-                node_src = edge[0]
-                node_dest = edge[1]
-                weight = edge[2]
-                graph.add_node(Node.new(node_src.to_i))
-                graph.add_node(Node.new(node_dest.to_i))
-                graph.add_edge(Edge.new(node_src.to_i, node_dest.to_i, weight.to_f))
-            end
-          end
-      end
-    return graph
   end
 end
 
 def main
   start = Time.now
-  graph = GraphLoader.instance.read_graph "g_with_negative_cycle.txt"
+  graph = GraphLoader.instance.read_graph "g_bellman_ford.txt"
   puts "Graph loaded in memory " +  (Time.now - start).to_s
   start = Time.now
   puts "Starting Bellman-Ford's algorithm"
@@ -75,7 +60,8 @@ def main
   initial_vertex = 1
   algorithm.execute(graph, initial_vertex) 
   puts "Bellman-Ford's took " +  (Time.now - start).to_s
-  puts "Bellman-Ford's result " +  algorithm.shortest_path.to_s
+  puts "Bellman-Ford's result -> " +  algorithm.shortest_path.to_s
+  puts "Graph has negative cycles -> " +  algorithm.has_negative_cycle.to_s
 end
 
 main
---
+++ week1/common.rb
@@ -0,0 +1,41 @@
+require 'singleton'
+
+FIXNUM_MAX = (2**(0.size * 8 -2) -1)
+FIXNUM_MIN = -(2**(0.size * 8 -2))
+
+class Path
+    attr_reader :src
+    attr_reader :dst
+    attr_reader :cost
+    def initialize(src,dst,cost)
+      @src = src
+      @dst = dst
+      @cost = cost
+    end
+    def to_s
+        "(" + @src.to_s + " -> " + @dst.to_s + " : " + cost.to_s + ")"
+      end
+end
+
+class GraphLoader
+    include Singleton
+    def read_graph filename
+      graph = Graph.new
+      File.open(filename, "r") do |f|
+          f.each_with_index do |line,index|
+              if (index == 0)
+                  next
+              else
+                  edge = line.split(/\s/).reject(&:empty?)
+                  node_src = edge[0]
+                  node_dest = edge[1]
+                  weight = edge[2]
+                  graph.add_node(Node.new(node_src.to_i))
+                  graph.add_node(Node.new(node_dest.to_i))
+                  graph.add_edge(Edge.new(node_src.to_i, node_dest.to_i, weight.to_f))
+              end
+            end
+        end
+      return graph
+    end
+  end
